804af8207ec8859c250cd70445419f1382b32d36
himzei <himzei@gmail.com>
2024-12-11 18:47:54 +0900

    re

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { useRef } from "react";
 
 function App() {
   const videoRef = useRef();
+  const canvasRef = useRef();
+
+  const [permissionGranted, setPermissionGranged] = useState(null);
+  const [videoStream, setVideoStream] = useState(null);
+
+  // console.log(videoRef);
+
+  useEffect(() => {
+    const requestCameraPermission = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        console.log(stream);
+        setPermissionGranged(true);
+        setVideoStream;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  }, []);
   return (
     <div className="max-w-xl w-full mx-auto">
       <h1 className="text-xl font-bold text-red-500 text-center py-4 border-b border-gray-300">
@@ -15,11 +38,11 @@ function App() {
           autoPlay
           playsInline
         ></video>
-        <canvas
+        {/* <canvas
           id="canvasElement"
           ref={canvasRef}
-          className="absolute w-full h-full"
-        ></canvas>
+          className="absolute w-full h-full border-2 rounded"
+        ></canvas> */}
       </div>
     </div>
   );
